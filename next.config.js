bf12ed10b22f98dcb3821f194861c8675fd4451f
chabonhamay4so9 <93881933+chabonhamay4so9@users.noreply.github.com>
2024-04-18 02:49:43 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    url:"https://bookpostponemoreover.com/k439c0hqe8?key=19fc64e0f360f39f6a49bb38c40675d1"
+    url:"https://rabbleit.com/v276qwq7sj?key=5f26839007fba1f0eb99906692fa87c5"
   },
 }
 
